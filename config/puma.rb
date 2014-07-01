94fc6dd4f4eac0ffb7bfaed1ec5a158a3144190b
Andrea Mazzini <andrea.mazzini@gmail.com>
2014-07-01 12:20:17 +0200

    Fixed puma

---
+++ config/puma.rb
@@ -0,0 +1,16 @@
+preload_app!
+
+min_threads = Integer(ENV['PUMA_MIN_THREADS'] || 5)
+max_threads = Integer(ENV['PUMA_MAX_THREADS'] || 5)
+
+threads min_threads, max_threads
+workers Integer(ENV['PUMA_WORKER_COUNT'] || 3 )
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    config = Rails.application.config.database_configuration[Rails.env]
+    config['reaping_frequency'] = Integer(ENV['DB_REAP_FREQ'] || 10)
+    config['pool']              = Integer(ENV['DB_POOL'] || 5)
+    ActiveRecord::Base.establish_connection(config)
+  end
+end
