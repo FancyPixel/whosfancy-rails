0338599256df669edfef711a2a8e9cd08a3b2ccd
Andrea Mazzini <andrea.mazzini@gmail.com>
2014-07-01 12:27:37 +0200

    fixed puma

--- config/puma.rb
+++ config/puma.rb
@@ -8,7 +8,7 @@ workers Integer(ENV['PUMA_WORKER_COUNT'] || 3 )
 
 on_worker_boot do
   ActiveSupport.on_load(:active_record) do
-    config = Rails.application.config.database_configuration[Rails.env]
+    config = ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
     config['reaping_frequency'] = Integer(ENV['DB_REAP_FREQ'] || 10)
     config['pool']              = Integer(ENV['DB_POOL'] || 5)
     ActiveRecord::Base.establish_connection(config)
